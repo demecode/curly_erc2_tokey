0d40f196d7b42aa5f46bbd737eb4cf4704460a0a
demecode <dpk.williams@gmail.com>
2021-12-08 10:46:59 +0000

    added deploy script which sets an initial token amount and should return the token name

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,11 +3,17 @@
 
 from os import access
 from scripts.helpers import get_account
+from brownie import TheToken
+from web3 import Web3
+
+initial_supply = Web3.toWei(1000, "ether")
 
 
 def deploy_tokey():
     account = get_account()
     print(account)
+    the_token = TheToken.deploy(initial_supply, {"from": account})
+    print(the_token.name())
     
 
 
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,4 +1,4 @@
-from brownie import network, accounts
+from brownie import network, accounts, config
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local", "ganache-local-3"]
 
@@ -10,3 +10,6 @@ def get_account(index=None, id=None):
         return accounts.load(id)
     if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
+    if network.show_active() in config["networks"]:
+        return accounts.add(config["wallets"]["from_key"])
+    return None
